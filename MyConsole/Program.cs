907ccfeff65658f66abf702bd7f1f201c2e0592d
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2024-07-02 13:31:32 -0400

    first commit

---
+++ MyConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MyConsole
+{
+    /// <summary>
+    /// Represents the entry point of the program.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The main method of the program.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int x;
+            for (x = 0; x <= 11; x++)
+            {
+                Console.WriteLine($"This is X = {x}");
+            }
+            for(int i = 1; i <= args.Length; i++)
+            {
+                Console.WriteLine($"Argument {i}: {args[i - 1]}");
+            }
+
+            Console.WriteLine("Done now!");
+        }
+    }
+}
